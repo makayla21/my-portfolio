1a4275f0d83966d4f0b98aa33fcb3d87c5b8aa01
Makayla Price <makaylaj1121@gmail.com>
2021-03-10 20:11:38 +0000

    Modify servlet to correctly convert list to JSONON string

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -9,15 +9,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-    ArrayList<String> messageList = new ArrayList<>(Arrays.asList("I dislike chocolate.", "I enjoy serial killer documentaries.", "I have a dog named Magic."));
+    ArrayList<String> messageList = new ArrayList<>(Arrays.asList("I dislike chocolate ", "I enjoy serial killer documentaries ", "I have a dog named Magic "));
+    String json = convertToJsonUsingGson(messageList);
+
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("Hello world!");
-    response.getWriter().println(messageList);
+    response.getWriter().println(json);
   }
+
+    private String convertToJsonUsingGson(ArrayList<String> messageList2) {
+        Gson gson = new Gson();
+        String json = gson.toJson(messageList2);
+        return json;
+    }
 }
