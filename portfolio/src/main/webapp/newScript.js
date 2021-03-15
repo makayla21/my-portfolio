cea1115ddd6a6005a082da808497bdee64b7b1fa
Makayla Price <makaylaj1121@gmail.com>
2021-03-15 03:10:57 +0000

    Change wording in java file and add Translation dependency

--- portfolio/src/main/webapp/newScript.js
+++ portfolio/src/main/webapp/newScript.js
@@ -1,4 +1,4 @@
-/**Fethces the response from the server and adds it to the page */
+/**Fetches the response from the server and adds it to the page */
 async function showResponse() {
     const responseFromServer = await fetch('/hello');
     const myObject = await responseFromServer.json();
